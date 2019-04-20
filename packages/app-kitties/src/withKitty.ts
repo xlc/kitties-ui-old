cea7fc47092661f7c6bb31f32c2d616d5fb4adf0
Bryan Chen <xlchen1291@gmail.com>
2019-04-20 21:25:51 +1200

    Display Kitty owner

--- packages/app-kitties/src/withKitty.ts
+++ packages/app-kitties/src/withKitty.ts
@@ -1,11 +1,15 @@
 import BN from 'bn.js';
-import { Option } from '@polkadot/types/codec';
+import { Option, AccountId } from '@polkadot/types';
 import { withCalls } from '@polkadot/ui-api/with';
 import { Kitty } from './types';
 
 export type Props = {
   kittyId: BN,
-  kitties_kitties?: Option<Kitty>
+  kitty?: Option<Kitty>,
+  owner?: Option<AccountId>
 };
 
-export default withCalls<Props>(['query.kitties.kitties', { paramName: 'kittyId' }]);
+export default withCalls<Props>(
+  ['query.kitties.kitties', { paramName: 'kittyId', propName: 'kitty' }],
+  ['query.kitties.kittyOwners', { paramName: 'kittyId', propName: 'owner' }]
+);
