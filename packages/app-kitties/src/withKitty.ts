c3f6f67db80a216930efaab910666b364050295a
Bryan Chen <xlchen1291@gmail.com>
2019-04-21 18:10:14 +1200

    Display price

--- packages/app-kitties/src/withKitty.ts
+++ packages/app-kitties/src/withKitty.ts
@@ -1,15 +1,17 @@
 import BN from 'bn.js';
-import { Option, AccountId } from '@polkadot/types';
+import { Option, AccountId, Balance } from '@polkadot/types';
 import { withCalls } from '@polkadot/ui-api/with';
 import { Kitty } from './types';
 
 export type Props = {
   kittyId: BN,
   kitty?: Option<Kitty>,
-  owner?: Option<AccountId>
+  owner?: Option<AccountId>,
+  price?: Option<Balance>
 };
 
 export default withCalls<Props>(
   ['query.kitties.kitties', { paramName: 'kittyId', propName: 'kitty' }],
-  ['query.kitties.kittyOwners', { paramName: 'kittyId', propName: 'owner' }]
+  ['query.kitties.kittyOwners', { paramName: 'kittyId', propName: 'owner' }],
+  ['query.kitties.kittyPrices', { paramName: 'kittyId', propName: 'price' }]
 );
